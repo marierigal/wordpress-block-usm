791db2cbdd84203ce4143f07271c7638fee98fb8
Marie Rigal <marierigal4@gmail.com>
2023-06-25 14:05:48 +0200

    style: format and lint

--- src/team-info/edit.js
+++ src/team-info/edit.js
@@ -1,24 +1,21 @@
-import { RichText, useBlockProps } from '@wordpress/block-editor';
+import { RichText, useBlockProps as blockProps } from '@wordpress/block-editor';
 
-export const edit = ({
-  attributes: { icon, title, content },
-  setAttributes,
-}) => {
-  const onChangeContent = (value) => {
-    setAttributes({ content: value });
+export const edit = ( { attributes: { icon, title, content }, setAttributes } ) => {
+  const onChangeContent = value => {
+    setAttributes( { content: value } );
   };
 
   return (
-    <div {...useBlockProps()}>
+    <div { ...blockProps() }>
       <div className="icon-wrapper">
-        <span className={`icon dashicons dashicons-${icon}`}></span>
+        <span className={ `icon dashicons dashicons-${ icon }` }></span>
       </div>
       <div className="content-wrapper">
-        <h3 className="title">{title}</h3>
+        <h3 className="title">{ title }</h3>
         <RichText
           className="content"
-          value={content}
-          onChange={onChangeContent}
+          value={ content }
+          onChange={ onChangeContent }
           tagName="ul"
           multiline="li"
         />
